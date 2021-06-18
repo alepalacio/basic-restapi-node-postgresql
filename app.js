98d501b33e00e98af0fe2b21d9a875ba50aee30b
alepalacio <alejandropalacio24@gmail.com>
2021-06-18 01:23:17 -0300

    first checkout

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const quotesRouter = require('./routes/quotes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('json spaces', 2)
+
+app.use('/', indexRouter);
+app.use('/quotes', quotesRouter);
+
+module.exports = app;
